90618beec1f38aca8d4f12481651bb33b0cc173b
durfi <danielvoros89@gmail.com>
2013-06-23 09:55:17 +0200

    Restoring freecell.js.

---
+++ freecell.js
@@ -0,0 +1,190 @@
+//set main namespace
+goog.provide('freecell');
+
+
+//get requirements
+goog.require('lime.Director');
+goog.require('lime.Scene');
+goog.require('lime.Layer');
+goog.require('lime.Label');
+goog.require('lime.GlossyButton');
+goog.require('lime.animation.Spawn');
+goog.require('lime.animation.FadeTo');
+goog.require('lime.animation.ScaleTo');
+goog.require('lime.animation.MoveTo');
+goog.require('freecell.Stack');
+goog.require('freecell.Card');
+goog.require('freecell.Deck');
+goog.require('freecell.Reserve');
+goog.require('freecell.Foundation');
+goog.require('freecell.LogEntry');
+
+freecell.WIDTH = 1280;
+freecell.HEIGHT = 768;
+
+freecell.STACK_COUNT = 8;
+freecell.STACK_COLOR = '#007000';
+freecell.STACK_GAP = 30;
+
+freecell.RESERVE_COUNT = 4;
+freecell.RESERVE_COLOR = '#007000';
+
+freecell.FOUNDATION_COUNT = 4;
+freecell.FOUNDATION_COLOR = '#00b000';
+
+freecell.CARD_WIDTH = 224;
+freecell.CARD_HEIGHT = 313;
+
+freecell.CARD_SUITS = {
+		CLUBS 	: 	0,
+		DIAMONDS:	1,
+		HEARTS	:	2,
+		SPADES	:	3
+};
+
+freecell.CARD_IMAGE = 'assets/cards2.png';
+
+freecell.callNewGame = function() {
+	freecell.newGame();
+};
+
+// entry point
+freecell.start = function(){
+	// M3W
+	var director;
+	if (typeof m3w === 'object') {
+		m3w.events.setCallback('start',freecell.callNewGame);
+		director = new lime.Director(m3w.container, freecell.WIDTH, freecell.HEIGHT);
+		$(m3w.container).css('background-color', '#008300');
+	} else {
+		director = new lime.Director(document.body, freecell.WIDTH, freecell.HEIGHT);
+	}
+	
+	director.makeMobileWebAppCapable();
+	// director.setDisplayFPS(false);
+	
+	// Create loading scene
+	var loadingScene = new lime.Scene;
+	var loadingLabel = new lime.Label().setSize(freecell.WIDTH, 80)
+		.setText('Loading game...')
+		.setFontSize(80)
+		.setFontColor("#fff")
+		.setAlign("center")
+		.setPosition(freecell.WIDTH / 2, freecell.HEIGHT / 2);
+	loadingScene.appendChild(loadingLabel);
+	
+
+	// Create game scene
+	var gameScene = new lime.Scene;
+	this.layer = new lime.Layer().setPosition(10, 0);
+	gameScene.appendChild(this.layer);
+	
+	// Create the buttons
+	this.btnNewGame = new lime.GlossyButton("Új játék").setSize(120, 40).setPosition(1180, 740);
+	goog.events.listen(this.btnNewGame,'click',function(e){
+	    freecell.newGame();
+	});
+	this.layer.appendChild(this.btnNewGame);
+	this.btnUndo = new lime.GlossyButton("Visszavonás").setSize(120, 40).setPosition(1040, 740);
+	goog.events.listen(this.btnUndo,'click',function(e){
+	    freecell.undo();
+	});
+	this.layer.appendChild(this.btnUndo);
+	
+	// Create the stacks
+	this.stacks = new Array();
+	for (var i = 0; i < freecell.STACK_COUNT; i ++) {
+		this.stacks[i] = new freecell.Stack(i, 120, 500, freecell.STACK_COLOR)
+			.setPosition(i * 150, 200);
+		this.layer.appendChild(this.stacks[i]);
+	}
+	
+	// Create the free cells
+	this.reserves = new Array();
+	for (var i = 0; i < freecell.RESERVE_COUNT; i ++) {
+		this.reserves[i] = new freecell.Reserve(i, 120, 160, freecell.RESERVE_COLOR)
+			.setPosition(i*150, 10);
+		this.layer.appendChild(this.reserves[i]);
+	}
+	
+	// Create the foundations
+	this.foundations = new Array();
+	for (var i = 0; i < freecell.FOUNDATION_COUNT; i ++) {
+		this.foundations[i] = new freecell.Foundation(i, 120, 160, freecell.FOUNDATION_COLOR)
+			.setPosition((i+freecell.RESERVE_COUNT)*150, 10);
+		this.layer.appendChild(this.foundations[i]);
+	}
+	
+	// Start a new game
+	freecell.newGame();
+	
+	// Loading scene while loading image
+	var img = new lime.fill.Image(freecell.CARD_IMAGE);
+	if (! img.isLoaded) {
+		director.replaceScene(loadingScene);
+		goog.events.listen(img, goog.events.EventType.LOAD, function() {
+			console.log("Image loaded.");
+			director.replaceScene(gameScene);
+		});
+	} else {
+		director.replaceScene(gameScene);
+	}
+
+};
+
+/**
+ * Undo last move
+ */
+freecell.undo = function () {
+	if (this.undoLog == null) 
+		return;
+	if (this.undoLog.length == 0)
+		return;
+	
+	var lastMove = this.undoLog.pop();
+	var cards = lastMove.to.SubStack(lastMove.card);
+	
+	// Move the cards!
+	for (var i = 0; i < cards.length; i ++) {
+		this.layer.setChildIndex(cards[i],this.layer.getNumberOfChildren()-1);
+		cards[i].MoveToStack(lastMove.from);
+	}
+};
+
+/**
+ * Start new game.
+ */
+freecell.newGame = function () {
+	// Create the log
+	this.undoLog = new Array();
+	
+	// Create the stacks
+	for (var i = 0; i < freecell.STACK_COUNT; i ++) {
+		this.stacks[i].cards = new Array();
+	}
+	
+	// Create the free cells
+	for (var i = 0; i < freecell.RESERVE_COUNT; i ++) {
+		this.reserves[i].card = null;
+	}
+	
+	// Create the foundations
+	for (var i = 0; i < freecell.FOUNDATION_COUNT; i ++) {
+		this.foundations[i].cards = new Array();
+	}
+	
+	// If this isn't the first game, delete the previous cards.
+	if (this.deck != null) {
+		for (var i = 0; i < this.deck.cards.length; i ++) {
+			this.layer.removeChild(this.deck.cards[i]);
+		}
+	}
+	
+	// Create, shuffle and deal the deck
+	this.deck = new freecell.Deck(this);
+	this.deck.Shuffle();
+	this.deck.Deal();
+};
+
+//this is required for outside access after code is compiled in ADVANCED_COMPILATIONS mode
+goog.exportSymbol('freecell.start', freecell.start);
