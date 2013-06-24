0ab79974707283efe699132c8ba1697668e38369
durfi <danielvoros89@gmail.com>
2013-06-24 09:20:32 +0200

    New output file name in compile script.

--- build_freecell.sh
+++ build_freecell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wd="`pwd`"
 cd ..
-python bin/lime.py build freecell -o freecell/compiled/fc.js
+python bin/lime.py build freecell -o freecell/compiled/freecell-c.js
 cp freecell/assets/* freecell/compiled/assets/
 cd "$wd"
