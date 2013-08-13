e1b84bdc9dafb2619167eccef00bab935857b9df
durfi <danielvoros89@gmail.com>
2013-08-13 08:27:56 +0200

    Build file changed, added server side script.

--- build_freecell.sh
+++ build_freecell.sh
@@ -3,5 +3,7 @@ wd="`pwd`"
 cd ..
 python bin/lime.py update freecell
 python bin/lime.py build freecell -o freecell/compiled/freecell-c.js
+cp freecell/compiled/freecell-c.js freecell/gapi/fc.js
 cp freecell/assets/* freecell/compiled/assets/
+cp freecell/assets/* freecell/gapi/assets/
 cd "$wd"
---
+++ gapi/update.php
@@ -0,0 +1,21 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$logging_directory = "log/";
+
+$post = $_POST;
+
+if(!isset($post['msg']) || !isset($post['usr']))
+  die("ERROR:No message received!");
+
+$filename = $logging_directory.$post['usr'].".solilog";
+if (!file_exists($filename))
+	$file = fopen($filename, 'w') or die("ERROR:Cannot open file");
+else
+	$file = fopen($filename, 'a') or die("ERROR:Cannot open file");
+fwrite($file,$post['msg']."\n");
+fclose($file);
+
+?>
