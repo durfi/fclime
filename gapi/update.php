7dcc30e95ef44ed7db20f007b6d39e765e05b68d
durfi <danielvoros89@gmail.com>
2013-09-19 16:15:39 +0200

    Fixed unnecessary backslashes bug on PHP side and renamed compiled .js in gapi.

--- gapi/update.php
+++ gapi/update.php
@@ -2,6 +2,7 @@
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('magic_quotes_gpc', 'off');
 
 $logging_directory = "log/";
 
@@ -15,7 +16,7 @@ if (!file_exists($filename))
 	$file = fopen($filename, 'w') or die("ERROR:Cannot open file");
 else
 	$file = fopen($filename, 'a') or die("ERROR:Cannot open file");
-fwrite($file,$post['msg']."\n");
+fwrite($file,stripslashes($post['msg'])."\n");
 fclose($file);
 
 ?>
